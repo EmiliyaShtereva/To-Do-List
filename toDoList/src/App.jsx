dec775d6528ff326202024b25d33c5855f02f4bb
EmiliyaShtereva <123276538+EmiliyaShtereva@users.noreply.github.com>
2024-05-21 23:01:35 +0300

    made template

--- toDoList/src/App.jsx
+++ toDoList/src/App.jsx
@@ -1,34 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='container'>
+      <div className='week-days'>
+        <div className='day'>Monday</div>
+        <div className='day'>Tuesday</div>
+        <div className='day'>Wednesday</div>
+        <div className='day'>Thursday</div>
+        <div className='day'>Friday</div>
+        <div className='day'>Saturday</div>
+        <div className='day'>Sunday</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='list'>
+      <div className='day-list'>
+        <div className='task'>
+          <p className='time'>1:00 - 2:00</p>
+          <p className='description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur suscipit sit illum dolor laborum ea, reprehenderit corrupti possimus in qui aspernatur asperiores, ipsa consequuntur itaque optio. Perspiciatis aliquid iusto quae.</p>
+        </div>
+        <div className='task'>
+          <p className='time'>1:00 - 2:00</p>
+          <p className='description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur suscipit sit illum dolor laborum ea, reprehenderit corrupti possimus in qui aspernatur asperiores, ipsa consequuntur itaque optio. Perspiciatis aliquid iusto quae.</p>
+        </div>
+        <div className='task'>
+          <p className='time'>1:00 - 2:00</p>
+          <p className='description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur suscipit sit illum dolor laborum ea, reprehenderit corrupti possimus in qui aspernatur asperiores, ipsa consequuntur itaque optio. Perspiciatis aliquid iusto quae.</p>
+        </div>
+        <div className='task'>
+          <p className='time'>1:00 - 2:00</p>
+          <p className='description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur suscipit sit illum dolor laborum ea, reprehenderit corrupti possimus in qui aspernatur asperiores, ipsa consequuntur itaque optio. Perspiciatis aliquid iusto quae.</p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        <div className='day-list'>
+        <div className='task'>
+          <p className='time'>1:00 - 2:00</p>
+          <p className='description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur suscipit sit illum dolor laborum ea, reprehenderit corrupti possimus in qui aspernatur asperiores, ipsa consequuntur itaque optio. Perspiciatis aliquid iusto quae.</p>
+        </div>
+        <div className='task'>
+          <p className='time'>1:00 - 2:00</p>
+          <p className='description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur suscipit sit illum dolor laborum ea, reprehenderit corrupti possimus in qui aspernatur asperiores, ipsa consequuntur itaque optio. Perspiciatis aliquid iusto quae.</p>
+        </div>
+        </div>
+        <div className='day-list'>
+        <div className='task'>
+          <p className='time'>1:00 - 2:00</p>
+          <p className='description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur suscipit sit illum dolor laborum ea, reprehenderit corrupti possimus in qui aspernatur asperiores, ipsa consequuntur itaque optio. Perspiciatis aliquid iusto quae.</p>
+        </div>
+        <div className='task'>
+          <p className='time'>1:00 - 2:00</p>
+          <p className='description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur suscipit sit illum dolor laborum ea, reprehenderit corrupti possimus in qui aspernatur asperiores, ipsa consequuntur itaque optio. Perspiciatis aliquid iusto quae.</p>
+        </div>
+        </div>
+        <div className='day-list'></div>
+        <div className='day-list'></div>
+        <div className='day-list'></div>
+        <div className='day-list'></div>
+      </div>
+    </div>
   )
 }
 
