f7c79e8b143d8bab2e0a3532c2116419dc72cd60
EmiliyaShtereva <123276538+EmiliyaShtereva@users.noreply.github.com>
2024-07-21 11:46:49 +0300

    added tasks from server to day list

--- toDoList/src/components/list/List.jsx
+++ toDoList/src/components/list/List.jsx
@@ -3,12 +3,23 @@ import DayList from "../day-list/DayList";
 import Details from "../details/Details";
 import Edit from "../edit/Edit";
 import styles from "./List.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import * as tasksService from '../../services/tasksService';
 
 export default function List() {
     const [showForm, setShowForm] = useState(false);
     const [showDetails, setShowDetails] = useState(false);
     const [showEdit, setShowEdit] = useState(false);
+    const [mondayTasks, setMondayTasks] = useState([]);
+
+    useEffect(() => {
+        tasksService.getMonday()
+            .then(result => {
+                setMondayTasks(result);
+            })
+            .catch(err => console.log(err))
+    }, []);
+
     const detailsClickHandler = () => {
         setShowDetails(true);
         setShowEdit(false);
@@ -24,30 +35,37 @@ export default function List() {
             <DayList
                 onPlus={() => setShowForm(true)}
                 onDetails={detailsClickHandler}
+                tasks={mondayTasks}
             />
             <DayList
                 onPlus={() => setShowForm(true)}
                 onDetails={detailsClickHandler}
+                tasks={mondayTasks}
             />
             <DayList
                 onPlus={() => setShowForm(true)}
                 onDetails={detailsClickHandler}
+                tasks={mondayTasks}
             />
             <DayList
                 onPlus={() => setShowForm(true)}
                 onDetails={detailsClickHandler}
+                tasks={mondayTasks}
             />
             <DayList
                 onPlus={() => setShowForm(true)}
                 onDetails={detailsClickHandler}
+                tasks={mondayTasks}
             />
             <DayList
                 onPlus={() => setShowForm(true)}
                 onDetails={detailsClickHandler}
+                tasks={mondayTasks}
             />
             <DayList
                 onPlus={() => setShowForm(true)}
                 onDetails={detailsClickHandler}
+                tasks={mondayTasks}
             />
 
             {showForm && <AddForm onClose={() => setShowForm(false)} />}
